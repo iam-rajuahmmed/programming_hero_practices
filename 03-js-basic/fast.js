2c05a44e91dfa46dac2d154cd51b549968ddba52
Raju Ahmmed <raju.paperles@gmail.com>
2026-07-19 11:44:14 +0600

    Basic and for loop

---
+++ 03-js-basic/fast.js
@@ -0,0 +1,28 @@
+// var A = 10;
+// var B = 20;
+// console.log(A + B);
+
+const price1 = 100;
+const price2 = 200;
+
+if(price1 === price2){
+    console.log("Prices are equal. I want to Buy this product");
+} else {
+    console.log("Prices are not equal");
+}
+if(price1 === price2){
+    console.log( "I want to buy this product");
+
+}else{
+    console.log("I don't want to buy this product");
+}
+
+if(price1 !=price2){
+    console.log("Prices are not equal");
+
+}
+if(price1 !==price2){
+    console.log("Price One is not equal to Price Two");
+}else{
+    console.log("Price One is equal to Price Two");
+}
---
+++ 03-js-basic/loop.js
@@ -0,0 +1,5 @@
+let a = 20;
+
+for (let i = 0; i < a; i++){
+    console.log(i + 1 +" Hello Bangladesh");
+}
